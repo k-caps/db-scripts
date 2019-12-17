54c8a38da509b3e51240d6bf577bc4869804d177
Kobi <kobirosenstein@gmail.com>
2019-12-17 14:24:15 +0200

    Create change_objects_ownership.sh

---
+++ Postgres/change_objects_ownership.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Usage:
+# All objects in <dbname> will be transferred to <user_name>
+# chmod +x ch_ownership.s && sh ch_ownership.sh -d dbname -user_name
+
+# Vars:
+WD=~/pg_change_ownership
+mkdir -p $WD
+SQLFILE=$WD/query_generator.sql
+DMLFILE=$WD/dmlcommands.sql
+DB_NAME=''
+USER_NAME=''
+while getopts "d:u:" arg; do
+        case $arg in 
+                d) DB_NAME="${OPTARG}";;
+                u) USER_NAME="${OPTARGE}";;
+        esac
+done
+
+# If you'd rather hardcode/ set the variables infile, do it here:
+#DB_NAME=<value>
+#USER_NAME=<value>
+
+printf "Building sqlfile..\n"
+# The "\o" causes  psql to save the genrated queries in the specified file.
+cat > $SQLFILE << EOF
+\o $DMLFILE
+EOF
+
+# Tables
+cat >> $SQLFILE << EOF
+select 'ALTER TABLE '||schemaname||'."'||tablename||'" OWNER TO $USER_NAME;' as "--Commands to run--"
+FROM pg_tables WHERE NOT schemaname IN ('pg_catalog','information_schema')
+UNION ALL
+EOF
+
+# FDW tables
+cat >> $SQLFILE << EOF
+select 'ALTER TABLE '||schemaname||'."'||tablename||'" OWNER TO $USER_NAME;'
+FROM information_schema.foreign_tables WHERE NOT foreign_table_schema IN ('pg_catalog','information_schema')
+EOF
+
+# Schemas
+cat >> $SQLFILE << EOF
+select 'ALTER SCHEMA "'||schema_name||'" OWNER TO $USER_NAME;'
+FROM information_schema.schemata WHERE NOT schema_name IN ('pg_catalog','information_schema')
+UNION ALL
+EOF
+
+# Sequences
+cat >> $SQLFILE << EOF
+select 'ALTER SEQUENCE '||sequence_schema||'."'||sequence_name||'" OWNER TO $USER_NAME;'
+FROM information_schema.sequences WHERE NOT sequence_schema IN ('pg_catalog','information_schema')
+UNION ALL
+EOF
+
+# Indexes
+cat >> $SQLFILE << EOF
+select 'ALTER INDEX "'||schemaname||'"."'||indexname||'" OWNER TO $USER_NAME;'
+FROM pg_indexes WHERE NOT schemaname in ('pg_catalog','information_schema')
+UNION ALL
+EOF
+
+# Extensions
+cat >> $SQLFILE << EOF
+select 'UPDATE pg_extension SET extowner=(SELECT oid FROM pg_authid WHERE rolname= ''$USER_NAME'') WHERE extname <> ''plpgsql'';'
+EOF
+### This is possibly necessary. If you need it, add it after the final query, and replace the ";" with the "union all" ###
+#UNION ALL select 'UPDATE pg_shdepend SET refobjid =(SELECT oid FROM pg_authid WHERE rolname = ''$USER_NAME'')
+#WHERE deptype=''o'' AND refobjid = (SELECT extowner FROM pg_extension WHERE extname <> ''plpgsql'');';
+
+printf "Ownership changes:\n"
+# Execute change of ownership:
+# Generates the ALTER commands into the dmlfile using the above queries
+psql -d $DB_NAME -U postgres -f $SQLFILE
+sed -i -e 's/^([0-9]\+.*//g' $DMLFILE
+cat $DMLFILE
+printf "Executing commands:\n"
+# Execute the generated queries
+psql -d $DB_NAME -U postgres -f $DMLFILE
+
+printf "\n\nDone.\n"
