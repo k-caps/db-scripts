3ac7e77b34d5801f3c989e42c4f248cbce5c494b
Kobi <kobirosenstein@gmail.com>
2020-02-03 09:59:36 +0200

    Delete functions.sql

--- Postgres/psql-custom-commands/functions.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION pg_schema_size(text) RETURNS BIGINT AS $$
-SELECT SUM(pg_total_relation_size(quote_ident(schemaname) || '.' || quote_ident(tablename)))::BIGINT FROM pg_tables WHERE schemaname = $1
-$$ LANGUAGE SQL;
-
-CREATE OR REPLACE FUNCTION sizedb() RETURNS text AS $$
-SELECT pg_size_pretty(pg_database_size(current_database()));
-$$ LANGUAGE SQL;
