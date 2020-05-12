aa7fc3d5a3947fcee086d427729be0d4d10b3b26
Kobi <kobirosenstein@gmail.com>
2020-05-12 10:38:42 +0300

    Create delete_useless_tablespace.sql

---
+++ Oracle/delete_useless_tablespace.sql
@@ -0,0 +1,8 @@
+-- sqlplus / as sysdba
+--select tablespace_name from dba_tablespaces;
+
+alter session set container = xepdb1;
+create undo tablespace undotbs02 datafile '/opt/oracle/oradata/XE/XEPDB1/undotbs02.dbf' size 24M autoextend off;
+alter system set undo_tablespace = undotbs02 scope=both;
+drop tablespace undotbs01;
+rm -f /opt/oracle/oradata/XE/XEPDB1/undotbs01.dbf
